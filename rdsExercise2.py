ee0213148cf8e9bdd5212ef9e1c672e23aa2fb0a
DiogooOliveira <diogooliveira6@hotmail.com>
2022-04-28 18:04:59 +0100

    Add files via upload

--- rdsExercise2.py
+++ rdsExercise2.py
@@ -4,9 +4,10 @@ from mininet.node import OVSSwitch, Controller, RemoteController, Ryu
 from mininet.topolib import TreeTopo
 from mininet.log import info
 from mininet.log import setLogLevel
-from mininet.link import TCLink
+from mininet.node import Node
 from subprocess import call
 from mininet.cli import CLI
+from mininet.link import TCLink, Intf
 
 
 
@@ -17,19 +18,20 @@ class RDSTopo( Topo ):
         "Desenvolvimento da topologia"
         
         net = Mininet( controller=RemoteController, switch=OVSSwitch, link=TCLink, waitConnected=True)
-                   
+
                                    
        
         # Criação do Controlador para ligacao ao Switch principal de Layer 3
         
-        c0 = net.addController( 'c0', port=6653, controller=RemoteController, ip='127.0.0.1', protocol='tcp')
+        c0 = net.addController( 'c0',  ip='127.0.0.1', port=6653, controller=RemoteController, mac='00:00:00:00:00:04', protocol='tcp')
         
-        # Criação do Controlador para ligacao ao Switch principal de Layer 3
+        # Criação do Controlador para ligacao aos Switchs de Layer 2
         
-        c1 = net.addController( 's2', port=6633, controller=RemoteController, ip='127.0.0.1', protocol='tcp')
-        c2 = net.addController( 's3', port=6633, controller=RemoteController, ip='127.0.0.1', protocol='tcp')
-        c3 = net.addController( 's4', port=6633, controller=RemoteController, ip='127.0.0.1', protocol='tcp')
-
+        c1 = net.addController( 'c1', port=6633, controller=RemoteController, ip='127.0.0.1', mac='00:00:00:00:00:05', protocol='tcp')
+        "c2 = net.addController( 's3', port=6633, controller=RemoteController, ip='127.0.0.1', protocol='tcp')"
+        "c3 = net.addController( 's4', port=6633, controller=RemoteController, ip='127.0.0.1', protocol='tcp')"
+        cmap = { 's1': c1, 's2': c1, 's3': c1, 's4': c1}
+        
 
        # Criação de Utilizadores com respetivo ip e Switchs de Layer 3 e Layer 2
         
@@ -38,21 +40,26 @@ class RDSTopo( Topo ):
         l2SwitchThree = self.addSwitch( 's3' )
         l2SwitchFour = self.addSwitch( 's4' )
         
-        hostOne = self.addHost( 'h1', ip='10.0.1.1', defaultRoute='via 10.0.1.254')
-        hostTwo = self.addHost( 'h2', ip='10.0.1.2', defaultRoute='via 10.0.1.254')
-        hostThree = self.addHost( 'h3', ip='10.0.1.3', defaultRoute='via 10.0.1.254')
+        # Rede A
         
-        hostFour = self.addHost( 'h4', ip='10.0.2.1', defaultRoute='via 10.0.2.254')
-        hostFive = self.addHost( 'h5', ip='10.0.2.6', defaultRoute='via 10.0.2.254' )
-        hostSix = self.addHost( 'h6', ip='10.0.2.10', defaultRoute='via 10.0.2.254' )
+        h1 = self.addHost( 'h1', ip='10.0.1.1', mac='10:00:00:00:00:01', defaultRoute='via 10.0.1.254')
+        h2 = self.addHost( 'h2', ip='10.0.1.2', mac='10:00:00:00:00:02', defaultRoute='via 10.0.1.254')
+        h2 = self.addHost( 'h3', ip='10.0.1.3', mac='10:00:00:00:00:03', defaultRoute='via 10.0.1.254')
         
+        # Rede B
         
-        hostSeven = self.addHost( 'h7', ip='10.0.3.6', defaultRoute='via 10.0.3.254')
-        hostEight = self.addHost( 'h8', ip='10.0.3.7', defaultRoute='via 10.0.3.254')
-        hostNine = self.addHost( 'h9', ip='10.0.3.9', defaultroute='via 10.0.2.254')
+        h4 = self.addHost( 'h4', ip='10.0.2.1', mac='20:00:00:00:00:01', defaultRoute='via 10.0.2.254')
+        h5 = self.addHost( 'h5', ip='10.0.2.6', mac='20:00:00:00:00:02', defaultRoute='via 10.0.2.254' )
+        h6 = self.addHost( 'h6', ip='10.0.2.10', mac='20:00:00:00:00:03', defaultRoute='via 10.0.2.254' )
+        
+        # Rede C
+        
+        h7 = self.addHost( 'h7', ip='10.0.3.6', mac='30:00:00:00:00:01', defaultRoute='via 10.0.3.254')
+        h8 = self.addHost( 'h8', ip='10.0.3.7', mac='30:00:00:00:00:02', defaultRoute='via 10.0.3.254')
+        h9 = self.addHost( 'h9', ip='10.0.3.9', mac='30:00:00:00:00:03', defaultroute='via 10.0.2.254')
         
 
-        
+
         # Ligação entre Switchs
         
         self.addLink ( 's1', 's2', delay='5ms') # Ligação Switch layer 3 com primeiro switch de layer 2 e perdas de 5ms
@@ -77,5 +84,12 @@ class RDSTopo( Topo ):
         self.addLink( 's4', 'h7' ) # Ligação Switch 3 e Utilizador 7
         self.addLink( 's4', 'h8' ) # Ligação Switch 3 e Utilizador 8
         self.addLink( 's4', 'h9', loss=10 ) # Ligação Switch 3 e Utilizador 9 com perdas em 10%
+        
+        
+        net.build()
+        
+        c0.start()
+        c1.start()
+        
 
 topos = { 'mytopo': ( lambda: RDSTopo() ) }
